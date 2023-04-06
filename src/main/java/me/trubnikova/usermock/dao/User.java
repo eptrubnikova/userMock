41727d0cb86d3776de53a2fdb856b5de9ce45eb1
eptrubnikova <eptrubnikova@gmail.com>
2023-04-06 15:45:25 +0600

    mockito

---
+++ src/main/java/me/trubnikova/usermock/dao/User.java
@@ -0,0 +1,11 @@
+package me.trubnikova.usermock.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String name;
+}
---
+++ src/test/java/me/trubnikova/usermock/dao/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package me.trubnikova.usermock.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class UserRepositoryTest {
+
+    private static final String TRUE_NAME1 = "user1";
+    private static final String TRUE_NAME2 = "user2";
+    private static final String TRUE_NAME3 = "user3";
+    private static final String TRUE_NAME4 = "user4";
+    private static final String TRUE_NAME5 = "user5";
+    private static final String NON_EXISTENT_USER = "not_user";
+    private static final HashSet<User> users = new HashSet<>(Arrays.asList(
+            new User(TRUE_NAME1),
+            new User(TRUE_NAME2), new User(TRUE_NAME3),
+            new User(TRUE_NAME4), new User(TRUE_NAME5)));
+    private final UserRepositoryImpl out = new UserRepositoryImpl();
+
+    @Test
+    void shouldCreateUserObjectWithCorrectParameters() {
+        User user = new User(TRUE_NAME1);
+        Assertions.assertEquals(TRUE_NAME1, out.getUserByName(user.getName()));
+    }
+
+    @Test
+    void shouldCreateUserObjectWithDefaultParameters() {
+        assertNull(out.getUserByName(NON_EXISTENT_USER));
+    }
+}
