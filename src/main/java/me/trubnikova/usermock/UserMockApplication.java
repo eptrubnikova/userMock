d7665eee293b59a2c2113f9ad580c8c67cd54691
eptrubnikova <eptrubnikova@gmail.com>
2023-04-06 15:37:32 +0600

    Initial commit

---
+++ src/main/java/me/trubnikova/usermock/UserMockApplication.java
@@ -0,0 +1,13 @@
+package me.trubnikova.usermock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/trubnikova/usermock/UserMockApplicationTests.java
@@ -0,0 +1,13 @@
+package me.trubnikova.usermock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
